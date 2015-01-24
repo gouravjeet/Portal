6ffc08aa64263005058d6e7430b0c045cff46510
gouravjeet <gouravjeet@gmail.com>
2015-01-24 10:58:49 -0500

    Adding table data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,36 @@
  * Controller of the portalApp
  */
 angular.module('portalApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope,$resource,$http) {
+    
+    $scope.data=[];
+    $scope.newdata={};
+    $scope.getData=function(){
+    	$http.get('https://api.mongolab.com/api/1/databases/dara/collections/data?apiKey=hQd3U3G0xCnfVikoy53-g6nG8J7smyhL').
+	    success(function(tagobj){
+	    	$scope.data=tagobj;
+	    	console.log($scope.data);
+	         
+	    })
+	    .error(function(){
+	    	console.log('error');
+	    });
+    };
+    $scope.getData();
+    $scope.save=function(){
+    	console.log($scope.newdata);
+    	var newdata = JSON.stringify($scope.newdata);
+    	$http.post('https://api.mongolab.com/api/1/databases/dara/collections/data?apiKey=hQd3U3G0xCnfVikoy53-g6nG8J7smyhL',newdata)
+    	.success(function(tagobj){
+    	$scope.data=tagobj;
+    	console.log($scope.data);
+    	$scope.newdata={};
+    	})
+	    .error(function(){
+	    	console.log('error');
+	    });
+	    $scope.getData();
+    };
+    
+
   });
