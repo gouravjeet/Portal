5950048a9f2c642f367bfdccef73cd0b824ef24b
gouravjeet <gouravjeet@gmail.com>
2015-01-24 11:05:17 -0500

    adding readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Portal
+
+Functionalities Working:
+1) Users will be able to see and search datasets of retail Stores.
+2) User will be able to add new retail store information. 
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,6 +12,7 @@ angular.module('portalApp')
     
     $scope.data=[];
     $scope.newdata={};
+    // This function is fetching data from mongoDB database
     $scope.getData=function(){
     	$http.get('https://api.mongolab.com/api/1/databases/dara/collections/data?apiKey=hQd3U3G0xCnfVikoy53-g6nG8J7smyhL').
 	    success(function(tagobj){
@@ -24,6 +25,8 @@ angular.module('portalApp')
 	    });
     };
     $scope.getData();
+    // This function is adding data into mongoDB database as a new dataset
+    // whenever a user enters information in input box
     $scope.save=function(){
     	console.log($scope.newdata);
     	var newdata = JSON.stringify($scope.newdata);
